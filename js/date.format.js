6e971a4c1dcde8b0d0a99f45fa029bb8b06eb47d
Jascha Dachtera <jascha.dachtera@googlemail.com>
2011-08-23 11:39:05 +0200

    Added Date German Stirngs

--- js/date.format.js
+++ js/date.format.js
@@ -124,5 +124,14 @@ Date.prototype.format = function (mask, utc) {
 	return dateFormat(this, mask, utc);
 };
 
-
+dateFormat.i18n = {
+	dayNames: [
+		"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa",
+		"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"
+	],
+	monthNames: [
+		"Jan", "Feb", "März", "Apr.", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez",
+		"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"
+	]
+};
 
